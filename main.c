0561f2288c2f780726bbe555065a63c715d65a7e
Derek P <pajarin.derek.b@gmail.com>
2025-05-11 17:35:58 +0800

    Added the --guide. Kindly check if it's good and/or let me if it should be revised etc.

--- main.c
+++ main.c
@@ -27,8 +27,59 @@ void printHelp(){
 }
 
 void printGuide(){
-    printf("WIP, please improve\n");
-    printf("Format: ./prog \"expression\" [pre|post|in] [-e]\n");
+    
+    printf("\n\n\nThe Simple Expression Converter is a utility used for converting a given arithmetic expression into its equivalent form in a different\n");
+    printf("notation. Arithmetic expressions can be represented using three different notations. Those are the infix, prefix, and postfix notations.");
+
+    printf("\n\nDEFINITIONS\n\n");
+    printf("Infix Notation\n");
+    printf("- The infix notation is the most commonly used representation for mathematical expressions. An expression is said to be in infix notation\n");
+    printf("when an operator is placed between two operands. Although infix notation is convenient and easy to read, it can be challenging for \n");
+    printf("computers to evaluate due to factors such as operator precedence and the existence of parentheses in the expression. \n");
+    printf("(Examples : \" 2 + 2 \" , \" X - Y \" , \"A / B\")\n\n\n");
+
+    printf("Prefix Notation\n");
+    printf("In prefix notation (also known as Polish Notation), expressions are represented by placing the operators before their operands. In this\n");
+    printf("representation, the position of the operands and operators determine the order of operations thus eliminating the dependency on\n");
+    printf("parentheses. (Examples : \"+ 2 2 \", \"- X Y \", \" / A B \")\n\n\n");
+
+    printf("Postfix Notation\n");
+    printf("- The postfix notation (also known as Reverse Polish Notation) represents mathematical expressions by having the operator succeed its\n");
+    printf("operands. Similar to prefix notation, postfix is also non-ambiguous as the order of operations can be determined from operator and operand\n");
+    printf("positions alone. It is most widely used by computers for evaluating arithmetic expressions.\n");
+    printf("(Examples : \" 2 2 + \", \" X Y - \", \" A B / \")\n\n\n");
+
+    printf("HOW EXPRESSION CONVERSION IS DONE\n\n");
+    printf("Input expressions taken from the command line are tokenized and checked for validity and type. An expression tree builder procedure is\n");
+    printf("then invoked for that particular expression's type. Since each notation is interpreted differently (i.e., infix parentheses\n");
+    printf("interpretation, prefix right-to-left scan), the utility has separate tree builders for each known notation.\n\n");
+
+    printf("Expression trees represent arithmetic expressions as binary trees where the operator acts as the parent or root node and its two operators\n");
+    printf("act as its children. Regardless of its type or notation used, any given expression can be converted to its equivalent in other notations\n");
+    printf("as long as its expression tree has been built. Conversion is primarily done by performing tree traversal techniques on the expression trees.\n\n");
+
+    printf("An in-order traversal of an expression tree results in the infix representation of an expression. This is due to its traversal behavior\n");
+    printf("where the left child node, parent/root node, and right child node are visited in that specific order. Prefix notation can also be achieved\n");
+    printf("by performing a pre-order traversal. This traversal visits the root node first and then its children from left to right. Finally, a post-\n");
+    printf("order traversal, where the left and right child nodes are visited first before the root node, produce the postfix notation.\n\n");
+
+
+    printf("HOW EXPRESSIONS ARE EVALUATED\n\n");
+    printf("The most efficient method of evaluating arithmetic expressions on a computer is by converting the said expression into postfix notation\n");
+    printf("and then evaluating it using the stack data structure. The evaluation process for postfix expressions is straightforward. If an incoming\n");
+    printf("character is an operand, it is pushed onto the stack. If it is an operator, two operands are popped from the stack, the operation is\n");
+    printf("applied to both operands, and the result is pushed back onto the stack. This process continues until only one element is left on the stack\n");
+    printf("which is the result of the evaluation.\n\n\n");
+
+
+    printf("SAMPLE USAGE\n\n");
+    printf("./prog					- Prints a simple help guide.\n");
+    printf("./prog 3+4*2				- Infix input. No conversion specified; defaults to prefix conversion.\n");
+    printf("./prog 3+4*2 post  			- Infix input. Conversion specified; converts to postfix.\n");
+    printf("./prog 3+(4*2) post			- Infix expression inputs with parentheses are also valid.\n");
+    printf("./prog 78+3- in				- Postfix input. Converts to infix.\n");
+    printf("./prog /+462 in -e			- Prefix input. Converts to infix. Evaluates the expression.\n\n\n");
+
 }
 
 void printType(int t) {
